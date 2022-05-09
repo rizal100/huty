cd341f9a026a31ba632c0c949546820455e8249a
rizal100 <rizalgamma@gmail.com>
2022-05-09 17:19:29 +0700

    memper baiki beberapa di gitub.lua

--- blajar.lua
+++ blajar.lua
@@ -1,7 +1,1 @@
 print("halogais")
-
-io.write(
-    "hello word,from",version,"!"
-)
-
---[[my firs program in lua--]]
--- gitub.lua
+++ gitub.lua
@@ -9,13 +9,15 @@ gg.clearResults()
 goto mulai
 
 ::mulai::
-menumulai = gg.choice({'nomorsatu','2 gigo','uang',},TES1,'silahkan di pilih')
+menumulai = gg.choice({'nomorsatu','2 gigo','uang','bukan main'},
+TES1,'keluar')
 
   if menumulai == 1 then goto A end
   if menumulai == 2 then goto B end
   if menumulai == 3 then goto C end
+  if menumulai == 4 then goto d end
   if menumulai == TEST1 then goto bye end
-  goto sdone
+  goto selesai
 
 ::A::
 
@@ -25,7 +27,7 @@ menumulai = gg.choice({'nomorsatu','2 gigo','uang',},TES1,'silahkan di pilih')
   gg.getResults(10)
   gg.editAll('124,567,897', gg.TYPE_DWORD, false, gg.SIGN_AQUAL, 0, -1)
   gg.toast('uang tidak terbatas')
-  goto sdone
+  goto selesai
 
 ::B::
 
@@ -35,7 +37,7 @@ menumulai = gg.choice({'nomorsatu','2 gigo','uang',},TES1,'silahkan di pilih')
   gg.getResults(10)
   gg.editAll('99999999999', gg.TYPE_FLOAT)
   gg.toast('uang tidak terbatas')
-  goto sdone
+  goto selesai
 
 ::C::
   gg.alert('jmeng hadeh')
@@ -56,13 +58,37 @@ menumulai = gg.choice({'nomorsatu','2 gigo','uang',},TES1,'silahkan di pilih')
     a[3].freezeType = gg.FREEZE_NORMAL
 print('addListItems: ', gg.addListItems(a))
 gg.toast('babi on')
-goto sdone
+goto selesai
 
-::bye::
-gg.toast('harussssi')
-goto sdone
+::d::
+gg.alert('tunggu saja')
+gg.clearResults()
+gg.searchNumber('243,356,455;-4,453,334,555',
+gg.TYPE_DWORD,false, 
+gg.SIGN_AQUAL, 0, -1)
+gg.searchNumber('435,344,678'
+gg.TYPE_DWORD, false,
+gg.SIGN_AQUAL, 0, -1)
+
+   a = gg.getResults(3) --load items))
+   a[1].value = '126,777,999'
+   a[1].freeze = true
+   a[1].freezeType = gg.FREEZE_NORMAL
+   a[2].value = '126,888,999'
+   a[2].freeze = true
+   a[3].freeze = 'freezeType' = gg.FREEZE_NORMAL
+   a[3].value = '126,777,999'
+   a[3].freeze = true
+   a[3].freezeType = gg.FREEZE_NORMAL
+
+print('addListItems: ', gg.addListItems(a))
+gg.toast('on')
+   goto selesai 
 
 
+::bye:: 
+gg.toast('keluar')
+goto selesai
 
-::sdonei::
+::selesai::
 os.exit()
